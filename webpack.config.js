691e90a210a145b35bfa61022350b631749fd2bb
redy <redy.aries12@gmail.com>
2021-03-17 22:59:03 -0500

    fixing the images one more time

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     // entry: {
     //     home:'./src/js/home.js',
     //     menu:'./src/js/menu.js',
